ca397901696dd729389ddd15240b9b36ea5e195c
NazarH <soundcfavorite@gmail.com>
2021-09-18 12:52:03 +0300

    commit20

--- StructuralPatterns/Adapter/Adapter.js
+++ StructuralPatterns/Adapter/Adapter.js
@@ -1,22 +1,45 @@
-class SomeIPhone {
-    useLightning() {
-        console.log('Use lightning port');
+class OldCalculator {
+    constructor() {
+        this.operations = function (term1, term2, operation) {
+            switch (operation) {
+                case 'add':
+                    return term1 + term2;
+                case 'sub':
+                    return term1 - term2;
+                default:
+                    return NaN;
+            }
+        };
     }
 }
-class AndroidPhone {
-    useTypeC() {
-        console.log('Use Type-C');
+class NewCalculator {
+    constructor() {
+        this.add = function (term1, term2) {
+            return term1 + term2;
+        };
+        this.sub = function (term1, term2) {
+            return term1 - term2;
+        };
     }
 }
-class LightningToTypeCAdapter {
-    constructor(android) {
-        this.androidDevise = android;
-    }
-    useLightning() {
-        console.log('Want to use lightning, converting to Type-C');
-        this.androidDevise.useTypeC();
+class AdapterNewToOldCalc {
+    constructor() {
+        const newCalc = new NewCalculator();
+        this.operations = function (term1, term2, operation) {
+            switch (operation) {
+                case 'add':
+                    return newCalc.add(term1, term2);
+                case 'sub':
+                    return newCalc.sub(term1, term2);
+                default:
+                    return NaN;
+            }
+        };
     }
 }
-let android = new AndroidPhone();
-let chargeAdapter = new LightningToTypeCAdapter(android);
-chargeAdapter.useLightning();
+const oldCalc = new OldCalculator();
+console.log(oldCalc.operations(10, 5, 'add')); // 15
+const newCalc = new NewCalculator();
+console.log(newCalc.add(10, 5)); // 15
+const adaptedCalc = new AdapterNewToOldCalc();
+console.log(adaptedCalc.operations(10, 5, 'add')); // 15;
--- StructuralPatterns/Adapter/Adapter.ts
+++ StructuralPatterns/Adapter/Adapter.ts
@@ -1,37 +1,59 @@
-interface IPhone{
-    useLightning();
-}
-
-interface Android{
-    useTypeC();
-}
-
-class SomeIPhone implements IPhone{
-    useLightning(){
-        console.log('Использовать lightning разъём');
+class OldCalculator {
+    operations: (term1: number, term2: number, operation: string) => number;
+    
+    constructor() {
+        this.operations = function(term1, term2, operation) {
+            switch (operation) {
+                case 'add':
+                    return term1 + term2;
+                case 'sub':
+                    return term1 - term2;
+                default:
+                    return NaN;
+            }
+        };
     }
 }
 
-class AndroidPhone implements Android{
-    useTypeC(){
-        console.log('Использовать Type-C');
+class NewCalculator {
+    add: (term1: number, term2: number) => number;
+    sub: (term1: number, term2: number) => number;
+
+    constructor() {
+        this.add = function(term1, term2) {
+            return term1 + term2;
+        };
+
+        this.sub = function(term1, term2) {
+            return term1 - term2;
+        };
     }
 }
 
-class LightningToTypeCAdapter implements IPhone{
-    androidDevise: Android;
+class AdapterNewToOldCalc {
+    operations: (term1: number, term2: number, operation: string) => number;
 
-    constructor(android: Android){
-        this.androidDevise = android;
-    }
+    constructor() {
+        const newCalc = new NewCalculator();
 
-    useLightning(){
-        console.log('Конвертировать lightning, в Type-C');
-        this.androidDevise.useTypeC();
+        this.operations = function(term1, term2, operation) {
+            switch (operation) {
+                case 'add':
+                    return newCalc.add(term1, term2);
+                case 'sub':
+                    return newCalc.sub(term1, term2);
+                default:
+                    return NaN;
+            }
+        };
     }
 }
 
-let android = new AndroidPhone();
-let chargeAdapter = new LightningToTypeCAdapter(android);
+const oldCalc = new OldCalculator();
+console.log(oldCalc.operations(10, 5, 'add')); // 15
 
-chargeAdapter.useLightning();
+const newCalc = new NewCalculator();
+console.log(newCalc.add(10, 5)); // 15
+
+const adaptedCalc = new AdapterNewToOldCalc();
+console.log(adaptedCalc.operations(10, 5, 'add')); // 15;
